c0824a2236c8064f8bca8b05e9f6eef327b1b240
Sanjay <sanjay@example.com>
2026-08-20 10:03:52 +0530

    test: temporarily render placeholder instead of redirect to test if index route is the issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    throw redirect({ to: "/admin/login" });
-  },
+  component: RootComponent,
 });
+
+function RootComponent() {
+  return <div>Loading...</div>;
+}
